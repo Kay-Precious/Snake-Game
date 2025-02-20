6f55866cc9b08b1f42b1fa2896d63c1e10a9f5e2
Kay Precious <kayprecious977@gmail.com>
2025-02-20 12:06:23 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import time
 from snake import Snake
 from food import Food
 from scoreboard import ScoreBoard
-
+# Screen set-up
 screen = Screen()
 
 screen.setup(600, 600)
@@ -11,29 +11,32 @@ screen.bgcolor("black")
 screen.tracer(0)
 screen.title("Snake game")
 
+# Module class call
 snake = Snake()
 food = Food()
 scoreboard = ScoreBoard()
 
+# Screen listening to keyboard keys set-up
 screen.listen()
 screen.onkey(snake.up, "Up")
 screen.onkey(snake.down, "Down")
 screen.onkey(snake.left, "Left")
 screen.onkey(snake.right, "Right")
 
+# Switch on/off
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
     snake.move()
 
-    # detect collision with wall if to wanted
+    # detect collision with wall
     if snake.head.distance(food) < 15:
         food.refresh()
         snake.extend()
         scoreboard.add_score()
 
-    # detect collision with wall
+    # detect collision with wall --- uncomment if wanted
     # if snake.head.xcor() > 290 or snake.head.xcor() < -290 or snake.head.ycor() > 290 or snake.head.ycor() < -290:
     #     scoreboard.reset()
 
